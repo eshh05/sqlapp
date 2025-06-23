c47a44b2c487906193a71740ba58b0a348592459
eshh05 <bb1230808@iitd.ac.in>
2025-06-23 10:11:52 +0530

    Update db.py

--- db.py
+++ db.py
@@ -8,11 +8,11 @@ load_dotenv()
 @st.cache_resource
 def get_connection():
     return psycopg2.connect(
-        dbname=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        host=os.getenv("DB_HOST"),
-        port=os.getenv("DB_PORT")
+        dbname=st.secrets["DB_NAME"],
+        user=st.secrets["DB_USER"],
+        password=st.secrets["DB_PASSWORD"],
+        host=st.secrets["DB_HOST"],
+        port=st.secrets["DB_PORT"]
     )
 
 def run_query(sql):
