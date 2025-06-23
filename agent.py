7b95ae52a9f4c29630ee371e787c46daf15b0a29
eshh05 <eshita2005@gmail.com>
2025-06-23 09:29:33 +0530

    Fully switched from dotenv to st.secrets

--- agent.py
+++ agent.py
@@ -1,12 +1,13 @@
 import os
 from openai import OpenAI
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
+import streamlit as st 
 
-load_dotenv()
+#load_dotenv()
 
-api_key = os.getenv("OPENAI_API_KEY")
-if not api_key:
-    raise ValueError("OPENAI_API_KEY not found in .env")
+api_key = st.secrets["OPENAI_API_KEY"]
+#if not api_key:
+    #raise ValueError("OPENAI_API_KEY not found in .env")
 
 client = OpenAI(api_key=api_key)
 
